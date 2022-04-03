8bd66e966f82b566084c794f2358a31b9eada136
Beau Collins <beau@collins.pub>
2022-04-03 09:44:29 -0700

    Upgrades

--- packages/request/src/request.ts
+++ packages/request/src/request.ts
@@ -1,32 +1,36 @@
-import { Result, isSuccess } from '@fracture/parse';
-import { IncomingMessage } from 'http';
-import { encode, ParsedUrlQueryInput } from 'querystring';
-import { format } from 'util';
-import { request as createRequest, RequestOptions } from 'https';
-import * as response from './response-handler';
-import * as request from './request-handler';
-import * as build from './request-builder';
+import { Result, isSuccess } from "@fracture/parse";
+import { IncomingMessage } from "http";
+import { encode, ParsedUrlQueryInput } from "querystring";
+import { format } from "util";
+import { request as createRequest, RequestOptions } from "https";
+import * as response from "./response-handler";
+import * as request from "./request-handler";
+import * as build from "./request-builder";
 
 export { response, request, build };
 
 export type ApiResponse<O> = {
-    result: O,
-    response: IncomingMessage,
-    request: RequestOptions,
-}
+  result: O;
+  response: IncomingMessage;
+  request: RequestOptions;
+};
 
-export type ApiRequest<T, R=any> = (options: T) => Promise<ApiResponse<R>>;
+export type ApiRequest<T, R = any> = (options: T) => Promise<ApiResponse<R>>;
 
 export type ApiResponseType<T> = T extends ApiResponse<infer U> ? U : never;
 
-export function asApiResponse<T>(request: RequestOptions, response: IncomingMessage, resolve: (result: ApiResponse<T>) => void): (result: T) => void {
-    return function(result) {
-        resolve({
-            result,
-            response,
-            request
-        });
-    }
+export function asApiResponse<T>(
+  request: RequestOptions,
+  response: IncomingMessage,
+  resolve: (result: ApiResponse<T>) => void
+): (result: T) => void {
+  return function (result) {
+    resolve({
+      result,
+      response,
+      request,
+    });
+  };
 }
 
 /**
@@ -34,24 +38,31 @@ export function asApiResponse<T>(request: RequestOptions, response: IncomingMess
  * Given options T and api response of type O, uses three functions to handle
  * the pipeline of sending an HTTP Request and receving a HTTP Respose.
  *
- * @param buildRequest RequetsBuilder<T>
+ * @param buildRequest RequestBuilder<T>
  * @param handleRequest RequestHandler<T>
  * @param handleResponse ResponseHandler<O>
  * @returns ApiRequest<T,O>
  */
-export function apiRequest<T, O>(buildRequest: build.RequestBuilder<T>, handleRequest: request.RequestHandler<T>, handleResponse: response.ResponseHandler<O>): ApiRequest<T,O> {
-    return function(config) {
-        const options = buildRequest(config, {});
-        return new Promise((resolve, reject) => {
-            handleRequest(
-                config,
-                createRequest(
-                    options,
-                    response => handleResponse(response, asApiResponse(options, response, resolve), reject)
-                ).on('error', reject)
-            );
-        });
-    }
+export function apiRequest<T, O>(
+  buildRequest: build.RequestBuilder<T>,
+  handleRequest: request.RequestHandler<T>,
+  handleResponse: response.ResponseHandler<O>
+): ApiRequest<T, O> {
+  return function (config) {
+    const options = buildRequest(config, {});
+    return new Promise((resolve, reject) => {
+      handleRequest(
+        config,
+        createRequest(options, (response) =>
+          handleResponse(
+            response,
+            asApiResponse(options, response, resolve),
+            reject
+          )
+        ).on("error", reject)
+      );
+    });
+  };
 }
 
 /**
@@ -59,12 +70,10 @@ export function apiRequest<T, O>(buildRequest: build.RequestBuilder<T>, handleRe
  *
  * @param buildRequest RequestBuilder<T>
  */
-export function getJson<T>(buildRequest: build.RequestBuilder<T>): ApiRequest<T> {
-    return apiRequest(
-        buildRequest,
-        request.emptyBody,
-        response.decodeJson
-    );
+export function getJson<T>(
+  buildRequest: build.RequestBuilder<T>
+): ApiRequest<T> {
+  return apiRequest(buildRequest, request.emptyBody, response.decodeJson);
 }
 
 /**
@@ -77,87 +86,85 @@ export function getJson<T>(buildRequest: build.RequestBuilder<T>): ApiRequest<T>
  * @return T => Promise<any>
  */
 export function putJson<T>(
-    buildRequest: build.RequestBuilder<T>,
-    encoder: (options: T) => any
+  buildRequest: build.RequestBuilder<T>,
+  encoder: (options: T) => any
 ): (config: T) => Promise<ApiResponse<any>> {
-    return jsonEncodedRequest(buildRequest, encoder, response.decodeJson);
+  return jsonEncodedRequest(buildRequest, encoder, response.decodeJson);
 }
 
-export function sendJson<T, O=any>(
-    buildRequest: build.RequestBuilder<T>,
-    encoder: (options: T) => any,
-    responseHandler: response.ResponseHandler<O>
+export function sendJson<T, O = any>(
+  buildRequest: build.RequestBuilder<T>,
+  encoder: (options: T) => any,
+  responseHandler: response.ResponseHandler<O>
 ): (config: T) => Promise<ApiResponse<O>> {
-    return jsonEncodedRequest(
-        buildRequest,
-        encoder,
-        responseHandler
-    )
+  return jsonEncodedRequest(buildRequest, encoder, responseHandler);
 }
 
 export function sendURLEncoded<T, O>(
-    buildRequest: build.RequestBuilder<T>,
-    encoder: (options: T) => ParsedUrlQueryInput,
-    responseHandler: response.ResponseHandler<O>
+  buildRequest: build.RequestBuilder<T>,
+  encoder: (options: T) => ParsedUrlQueryInput,
+  responseHandler: response.ResponseHandler<O>
 ): (config: T) => Promise<ApiResponse<O>> {
-    return urlEncodedRequest(
-        buildRequest,
-        encoder,
-        responseHandler
-    );
+  return urlEncodedRequest(buildRequest, encoder, responseHandler);
 }
 
 export function jsonEncodedRequest<T, O>(
-    buildRequest: build.RequestBuilder<T>,
-    encoder: (options: T) => any,
-    handleResponse: response.ResponseHandler<O>
+  buildRequest: build.RequestBuilder<T>,
+  encoder: (options: T) => any,
+  handleResponse: response.ResponseHandler<O>
 ): (config: T) => Promise<ApiResponse<O>> {
-    return requestWithBody(
-        buildRequest,
-        mapBuffer(mapEncoder(encoder, JSON.stringify), 'application/json'),
-        handleResponse
-    );
+  return requestWithBody(
+    buildRequest,
+    mapBuffer(mapEncoder(encoder, JSON.stringify), "application/json"),
+    handleResponse
+  );
 }
 
-function mapBuffer<O, T, S extends string>(encoder: (options: O) => T, contentType: S ) {
-    return (options: O): [Buffer, S] => {
-        return [Buffer.from(encoder(options)), contentType]
-    }
+function mapBuffer<O, T extends string, S extends string>(
+  encoder: (options: O) => T,
+  contentType: S
+) {
+  return (options: O): [Buffer, S] => {
+    return [Buffer.from(encoder(options)), contentType];
+  };
 }
 
-function mapEncoder<T, A, B>(encoder: (options: T) => A, map: (encoded: A) => B): (options: T) => B {
-    return options => map(encoder(options));
+function mapEncoder<T, A, B>(
+  encoder: (options: T) => A,
+  map: (encoded: A) => B
+): (options: T) => B {
+  return (options) => map(encoder(options));
 }
 
 export function urlEncodedRequest<T, O>(
-    buildRequest: build.RequestBuilder<T>,
-    encoder: (options: T) => ParsedUrlQueryInput,
-    handleResponse: response.ResponseHandler<O>
+  buildRequest: build.RequestBuilder<T>,
+  encoder: (options: T) => ParsedUrlQueryInput,
+  handleResponse: response.ResponseHandler<O>
 ): (config: T) => Promise<ApiResponse<O>> {
-    return requestWithBody(
-        buildRequest,
-        mapBuffer(mapEncoder(encoder, encode), 'application/x-www-form-urlencoded'),
-        handleResponse
-    );
+  return requestWithBody(
+    buildRequest,
+    mapBuffer(mapEncoder(encoder, encode), "application/x-www-form-urlencoded"),
+    handleResponse
+  );
 }
 
 export function requestWithBody<T, O>(
-    buildRequest: build.RequestBuilder<T>,
-    encoder: (options: T) => [Buffer, string],
-    handleResponse: response.ResponseHandler<O>
+  buildRequest: build.RequestBuilder<T>,
+  encoder: (options: T) => [Buffer, string],
+  handleResponse: response.ResponseHandler<O>
 ): (config: T) => Promise<ApiResponse<O>> {
-    return function(config: T) {
-        const [buffer, contentType] = encoder(config);
-        return apiRequest(
-            build.build(
-                buildRequest,
-                build.addHeader('content-length', () => String(buffer.length)),
-                build.addHeader('content-type', () => contentType)
-            ),
-            request.writeBuffer(buffer),
-            handleResponse
-        )(config);
-    }
+  return function (config: T) {
+    const [buffer, contentType] = encoder(config);
+    return apiRequest(
+      build.build(
+        buildRequest,
+        build.addHeader("content-length", () => String(buffer.length)),
+        build.addHeader("content-type", () => contentType)
+      ),
+      request.writeBuffer(buffer),
+      handleResponse
+    )(config);
+  };
 }
 
 /**
@@ -166,31 +173,46 @@ export function requestWithBody<T, O>(
  * @param response ApiResponse<T>
  * @return ApiResponse<T>
  */
-export function requireSuccess<T>(response: ApiResponse<T>): Promise<ApiResponse<T>> {
-    if (response.response.statusCode === undefined) {
-        return Promise.reject(new Error('Missing status code'));
-    }
-    if (response.response.statusCode >= 300) {
-        return Promise.reject(new Error(`Failed: ${response.request.method} ${response.request.host}${response.request.path} ${response.response.statusCode} ${JSON.stringify(response.result)}`));
-    }
-    return Promise.resolve(response);
+export function requireSuccess<T>(
+  response: ApiResponse<T>
+): Promise<ApiResponse<T>> {
+  if (response.response.statusCode === undefined) {
+    return Promise.reject(new Error("Missing status code"));
+  }
+  if (response.response.statusCode >= 300) {
+    return Promise.reject(
+      new Error(
+        `Failed: ${response.request.method} ${response.request.host}${
+          response.request.path
+        } ${response.response.statusCode} ${JSON.stringify(response.result)}`
+      )
+    );
+  }
+  return Promise.resolve(response);
 }
 
-export function requireStatusCode<T>(statusCode: number, ...statusCodes: number[]): (response: ApiResponse<T>) => Promise<ApiResponse<T>> {
-    const acceptedCodes: {[code: number]: true|undefined} = [statusCode, ...statusCodes].reduce(
-        (codes, code) => ({...codes, [code]: true}),
-        {}
-    );
-    return (response) => {
-        const responseStatusCode = response.response.statusCode;
-        if (!responseStatusCode) {
-            return Promise.reject(new Error('Missing status code'));
-        }
-        if (!acceptedCodes[responseStatusCode]) {
-            return Promise.reject(new Error(`Unexpected status code: ${responseStatusCode} ${response.request.method} ${response.request.host}${response.request.path}`));
-        }
-        return Promise.resolve(response);
+export function requireStatusCode<T>(
+  statusCode: number,
+  ...statusCodes: number[]
+): (response: ApiResponse<T>) => Promise<ApiResponse<T>> {
+  const acceptedCodes: { [code: number]: true | undefined } = [
+    statusCode,
+    ...statusCodes,
+  ].reduce((codes, code) => ({ ...codes, [code]: true }), {});
+  return (response) => {
+    const responseStatusCode = response.response.statusCode;
+    if (!responseStatusCode) {
+      return Promise.reject(new Error("Missing status code"));
     }
+    if (!acceptedCodes[responseStatusCode]) {
+      return Promise.reject(
+        new Error(
+          `Unexpected status code: ${responseStatusCode} ${response.request.method} ${response.request.host}${response.request.path}`
+        )
+      );
+    }
+    return Promise.resolve(response);
+  };
 }
 
 /**
@@ -200,26 +222,29 @@ export function requireStatusCode<T>(statusCode: number, ...statusCodes: number[
  * @return ApiResponse<T>
  */
 export function logStatus<T>(response: ApiResponse<T>): ApiResponse<T> {
-    console.warn('Response, Status', response.response.statusCode);
-    return response;
+  console.warn("Response, Status", response.response.statusCode);
+  return response;
 }
 
 export function requireValid<T>(result: Result<T>): Promise<T> {
-	return isSuccess(result)
-		? Promise.resolve(result.value)
-		: Promise.reject(new Error(result.reason));
+  return isSuccess(result)
+    ? Promise.resolve(result.value)
+    : Promise.reject(new Error(result.reason));
 }
 
-export function requireValidResponse<T>(response: ApiResponse<Result<T>>): Promise<T> {
-    return requireValid(response.result);
+export function requireValidResponse<T>(
+  response: ApiResponse<Result<T>>
+): Promise<T> {
+  return requireValid(response.result);
 }
 
 export function mapRequestOptions<A>(mapOptions: <T>(options: T) => A) {
-    return <X,O>(api: ApiRequest<A,O>): ApiRequest<X,O> =>
-        options =>  api(mapOptions(options))
+  return <X, O>(api: ApiRequest<A, O>): ApiRequest<X, O> =>
+    (options) =>
+      api(mapOptions(options));
 }
 
 export function logResponse<T>(response: ApiResponse<T>): ApiResponse<T> {
-    process.stderr.write(format('Result\n', response.result));
-    return response;
+  process.stderr.write(format("Result\n", response.result));
+  return response;
 }
--- packages/request/src/response-handler.ts
+++ packages/request/src/response-handler.ts
@@ -1,58 +1,85 @@
-import { IncomingMessage } from 'http';
-import { Result, Parser, failure, success, mapParser, mapFailure } from '@fracture/parse';
+import { IncomingMessage } from "http";
+import {
+  Result,
+  Parser,
+  failure,
+  success,
+  mapParser,
+  mapFailure,
+} from "@fracture/parse";
 
-export type ResponseHandler<T> = (response: IncomingMessage, resolve: (result: T) => void, reject: (error: Error) => void) => void;
+export type ResponseHandler<T> = (
+  response: IncomingMessage,
+  resolve: (result: T) => void,
+  reject: (error: Error) => void
+) => void;
 
 export function collectBuffers(): ResponseHandler<Array<Buffer>> {
-    return function(res, onResult, onError) {
-        const buffers: Array<Buffer> = [];
-        res.on('data', function(data) {
-            buffers.push(data);
-        });
-        res.on('end', function() {
-            onResult(buffers);
-        });
-        res.on('error', onError);
-    }
+  return function (res, onResult, onError) {
+    const buffers: Array<Buffer> = [];
+    res.on("data", function (data) {
+      buffers.push(data);
+    });
+    res.on("end", function () {
+      onResult(buffers);
+    });
+    res.on("error", onError);
+  };
 }
 
-export const withData = mapHandler(collectBuffers(), buffers => Buffer.concat(buffers).toString('utf-8'));
+export const withData = mapHandler(collectBuffers(), (buffers) =>
+  Buffer.concat(buffers).toString("utf-8")
+);
 
 export const decodeJson = mapHandler(collectBuffers(), (buffers, res) => {
-    try {
-        return JSON.parse(Buffer.concat(buffers).toString('utf8'));
-    } catch (error) {
-        console.error('failed to parse', res.statusCode, Buffer.concat(buffers).toString('utf8'));
-        throw error;
-    }
+  try {
+    return JSON.parse(Buffer.concat(buffers).toString("utf8"));
+  } catch (error) {
+    console.error(
+      "failed to parse",
+      res.statusCode,
+      Buffer.concat(buffers).toString("utf8")
+    );
+    throw error;
+  }
 });
 
-export function mapHandler<A, B>(handler: ResponseHandler<A>, map: (result: A, res: IncomingMessage) => B): ResponseHandler<B> {
-    return function(res, onResult, onError) {
-        return handler(res, (a) => {
-            try {
-                onResult(map(a, res));
-            } catch (error) {
-                onError(error);
-            }
-        }, onError);
-    }
+export function mapHandler<A, B>(
+  handler: ResponseHandler<A>,
+  map: (result: A, res: IncomingMessage) => B
+): ResponseHandler<B> {
+  return function (res, onResult, onError) {
+    return handler(
+      res,
+      (a) => {
+        try {
+          onResult(map(a, res));
+        } catch (error) {
+          if (error instanceof Error) {
+            onError(error);
+            return;
+          }
+          throw error;
+        }
+      },
+      onError
+    );
+  };
 }
 
 function parseJson(raw: string): Result<any, string> {
-    try {
-        return success(JSON.parse(raw));
-    } catch (error) {
-        return failure(raw, error.message);
+  try {
+    return success(JSON.parse(raw));
+  } catch (error) {
+    if (error instanceof Error) {
+      return failure(raw, error.message);
     }
+    throw error;
+  }
 }
 
-export function withParser<O,I>(parse: Parser<O,I>): Parser<O, string> {
-	return mapParser(
-        parseJson,
-        value => mapFailure(
-            parse(value),
-            (failure) => ({...failure, value})
-        )
-    );
+export function withParser<O, I>(parse: Parser<O, I>): Parser<O, string> {
+  return mapParser(parseJson, (value) =>
+    mapFailure(parse(value), (failure) => ({ ...failure, value }))
+  );
 }
